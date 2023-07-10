02e0f07049f795d24d3fb5f8a28df03e5b1fb430
David Härer <david@haerer.dev>
2023-07-10 23:41:35 +0200

    notes/config.py

--- notes/config.py
+++ notes/config.py
@@ -1,6 +1,7 @@
+from datetime import datetime
+from pathlib import Path
 
 from pydantic import BaseSettings
-from pathlib import Path
 
 
 class Config(BaseSettings):
@@ -18,11 +19,21 @@ class Config(BaseSettings):
         env_file = Path().absolute() / ".env"
         env_file_encoding = "utf-8"
         # secrets_dir = "/run/secrets"
+        
+    @property
+    def db_path(self):
+        path = self.data_path / "notes.db"
+        return path.resolve()
+    
+    @property
+    def backup_db_path(self):
+        now = datetime.utcnow().isoformat()
+        path = self.data_path / f"notes__{now}.db"
+        return path.resolve()
 
     @property
     def db_url(self):
-        db_path = self.data_path / "notes.db"
-        return f"sqlite:///{db_path.resolve()}"
+        return f"sqlite:///{self.db_path}"
 
     @property
     def json_path(self):
